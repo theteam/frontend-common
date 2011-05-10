b334ee3cc4dd96f2246cb0edef1e21d04a4d2621
jakearchibald <jakearchibald@theteam.co.uk>
2011-05-10 05:16:12 -0700

    Added looping

--- script/ScrollPane.js
+++ script/ScrollPane.js
@@ -20,6 +20,8 @@ window.theTeam || (window.theTeam = {});
 			duration - Duration for the animation in ms, default: 500
 			easing - Easing function, default: swing
 			fade - fade during transition, default: false
+			loop - default: Don't loop
+						'item' - No gaps, 1st item will appear in the space after the last item
 	
 	properties:
 		itemsPerPage - Number of items per page. This is equal to the number of
@@ -44,14 +46,15 @@ theTeam.ScrollPane = (function() {
 			margin: 0,
 			duration: 500,
 			easing: 'swing',
-			fade: false
+			fade: false,
+			loop: ''
 		}, opts || {});
 		
 		this.currentPage = 1;
 		this._$itemContainer = $itemContainer;
 		this._$pane = $('<div class="scroll-pane" />').css({ overflow: 'hidden', position: 'relative' }).insertBefore($itemContainer).append($itemContainer);
 		this._$items = $itemContainer.children();
-		this.itemsPerPage = getItemsPerPage(this._$items); // defer this to first usage?
+		this.itemsPerPage = getItemsPerPage(this._$items);
 		this.totalPages = Math.ceil( this._$items.length / this.itemsPerPage );
 		this._positions = []; // positions for each visible item in its 'visible' position. Array of {top, left}
 		this._paneWidth = 0; // set correctly in _fixPositions
@@ -71,13 +74,14 @@ theTeam.ScrollPane = (function() {
 		scrollPane._paneWidth = scrollPane._$pane.width();
 		scrollPane._$itemContainer.staticSize();
 		
-		scrollPane._$currentItems = scrollPane._$items.slice(0, scrollPane.itemsPerPage).each(function() {
+		// this is an array rather than jq as for looping carousels, we may want to represent them in a different order to the DOM, & jq likes to reorder
+		scrollPane._currentItems = scrollPane._$items.slice(0, scrollPane.itemsPerPage).each(function() {
 			var $this = $(this),
 				position = $this.position();
 			
 			scrollPane._positions.push(position);
 			$this.css(position);
-		});
+		}).toArray();
 		
 		scrollPane._$itemContainer.css({
 			position: 'absolute',
@@ -91,11 +95,14 @@ theTeam.ScrollPane = (function() {
 	
 	// Reset the positions for the current page
 	ScrollPaneProto._resetPos = function() {
-		var start = this.itemsPerPage * (this.currentPage - 1),
-			scrollPane = this;
+		var	scrollPane = this,
+			currentItems = scrollPane._currentItems;
 		
-		this._$items.hide().slice(start, start + this.itemsPerPage).show().each(function(i) {
-			this.style.left = scrollPane._positions[i].left + 'px';
+		scrollPane._$items.hide();
+		$(currentItems).show();
+		
+		$.each(currentItems, function(i, elm) {
+			elm.style.left = scrollPane._positions[i].left + 'px';
 		});
 		
 		this._$itemContainer.css('left', 0);
@@ -108,71 +115,109 @@ theTeam.ScrollPane = (function() {
 		
 		var scrollPane = this,
 			moveBack = 'forward' in opts ? !opts.forward : pageNum < scrollPane.currentPage,
-			start = scrollPane.itemsPerPage * (pageNum - 1),
-			$nextItems = scrollPane._$items.slice(start, start + scrollPane.itemsPerPage),
-			duration = scrollPane._opts.duration,
-			easing = scrollPane._opts.easing;
+			start = scrollPane.itemsPerPage * (pageNum - 1) % scrollPane._$items.length,
+			nextItems = scrollPane._getNextItems(start);
 		
-		scrollPane._posForAnim($nextItems, moveBack);
+		console.log(nextItems);
+		
+		scrollPane._posForAnim(nextItems, moveBack);
 		scrollPane.busy = true;
 		
 		scrollPane._$itemContainer.animate({
 			left: (scrollPane._paneWidth + scrollPane._opts.margin) * (moveBack ? 1 : -1)
 		}, {
-			duration: duration,
-			easing: easing,
+			duration: scrollPane._opts.duration,
+			easing: scrollPane._opts.easing,
 			complete: function() {
 				scrollPane.busy = false;
 				scrollPane._resetPos();
 			}
 		});
 		
-		if ( scrollPane._opts.fade ) {
-			$nextItems.css('opacity', 0).animate({
-				opacity: 1
-			}, {
-				duration: duration,
-				easing: easing,
-				complete: function() {
-					$(this).css('filter', '');
-				}
-			});
-			
-			scrollPane._$currentItems.animate({
-				opacity: 0
-			}, {
-				duration: duration,
-				easing: easing
-			});
-		}
+		// fade in/out
+		scrollPane._opts.fade && scrollPane._fadeItems(nextItems, scrollPane._currentItems);
 		
 		scrollPane.currentPage = pageNum;
-		scrollPane._$currentItems = $nextItems;
+		scrollPane._currentItems = nextItems;
 		return this;
 	};
 	
+	// returns array rather than jQuery, as jQuery forces DOM ordering, which breaks looping
+	ScrollPaneProto._getNextItems = function(start) {
+		// fix negatives
+		if (start < 0) {
+			start = this._$items.length + start;
+		}
+		
+		// normalise for looping
+		var itemsPerPage = this.itemsPerPage,
+			nextItems = this._$items.slice(start, start + itemsPerPage).toArray(),
+			gaps = itemsPerPage - nextItems.length;
+		
+		if (gaps && this._opts.loop == 'item') {
+			nextItems = nextItems.concat( this._$items.toArray().slice(0, gaps) );
+		}
+		
+		return nextItems;
+	};
+	
+	ScrollPaneProto._fadeItems = function(nextItems, currentItems) {
+		var duration = this._opts.duration,
+			easing = this._opts.easing;
+		
+		$(nextItems).css('opacity', 0).animate({
+			opacity: 1
+		}, {
+			duration: duration,
+			easing: easing,
+			complete: function() {
+				$(this).css('filter', '');
+			}
+		});
+		
+		$(currentItems).animate({
+			opacity: 0
+		}, {
+			duration: duration,
+			easing: easing
+		});
+	};
+	
 	ScrollPaneProto.next = function() {
-		if (this.busy || this.currentPage === this.totalPages ) { return this; }
+		if (
+			this.busy ||
+			// if there's no next page & we're not looping
+			(!this._opts.loop && this.currentPage === this.totalPages)
+		) { return this; }
+		
 		this.changeTo( this.currentPage + 1 );
 		return this;
 	};
 	
 	ScrollPaneProto.prev = function() {
-		if (this.busy || this.currentPage === 1 ) { return this; }
+		if (
+			this.busy ||
+			// if there's no next page & we're not looping
+			(!this._opts.loop && this.currentPage === 1)
+		) { return this; }
 		this.changeTo( this.currentPage - 1 );
 		return this;
 	};
 	
-	ScrollPaneProto._posForAnim = function($nextItems, moveBack) {
+	ScrollPaneProto._posForAnim = function(nextItems, moveBack) {
 		var scrollPane = this;
 		
-		$nextItems.show().each(function(i) {
-			var style = this.style,
+		$(nextItems).show();
+		
+		$.each(nextItems, function(i, elm) {
+			var style = elm.style,
 				pos = scrollPane._positions[i];
 				
 			style.top = pos.top + 'px';
 			style.left = (scrollPane._paneWidth + scrollPane._opts.margin) * (moveBack ? -1 : 1) + pos.left + 'px';
-		}).eq(0).addClass('first');
+		});
+		
+		$( nextItems[nextItems.length - 1] ).addClass('first');
 	};
 	
 	function getItemsPerPage($children) {
